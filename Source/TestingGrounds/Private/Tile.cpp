74ba0bdb53417b2de72cc1f9c7a0fc083c13f8ee
Sargonius-WORK <evgeniy@rplay.ru>
2017-12-13 20:35:11 +0300

    290 - Level items generation, sphere casts, 500mb+ of assets without lfs :D

---
+++ Source/TestingGrounds/Private/Tile.cpp
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tile.h"
+#include "DrawDebugHelpers.h"
+
+
+// Sets default values
+ATile::ATile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CastSphere(GetActorLocation(), 300);
+	
+}
+
+// Called every frame
+void ATile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ATile::PlaceActors(TSubclassOf<AActor> ToSpawn, int32 MinSpawn, int32 MaxSpawn)
+{
+	FVector Min = FVector(0, -2000, 0);
+	FVector Max = FVector(4000, 2000, 0);
+	FBox Bound(Min, Max);
+	int32 NumberToSpawn = FMath::RandRange(MinSpawn, MaxSpawn);
+	for (size_t i = 0; i < NumberToSpawn; i++)
+	{
+		FVector SpawnPoint = FMath::RandPointInBox(Bound);
+		AActor* Spawned = GetWorld()->SpawnActor(ToSpawn);
+		Spawned->SetActorRelativeLocation(SpawnPoint);
+		Spawned->AttachToActor(this, FAttachmentTransformRules(EAttachmentRule::KeepRelative, false));
+	}
+	
+	//UE_LOG(LogTemp, Warning, TEXT("Point: %s"), *SpawnPoint.ToString());
+}
+
+bool ATile::CastSphere(FVector Location, float Radius)
+{
+	FHitResult HitResult;
+	bool HasHit =  GetWorld()->SweepSingleByChannel(
+		HitResult,
+		Location,
+		Location,
+		FQuat::Identity,
+		ECollisionChannel::ECC_Camera,
+		FCollisionShape::MakeSphere(Radius));
+
+	FColor ResultColor = HasHit ? FColor::Red : FColor::Green;
+	DrawDebugSphere(GetWorld(), Location, Radius, 10, ResultColor, true, 100);
+
+	return HasHit;
+}
+
